f99e2f0ef6ddc4a37938403ed5e0d1266c4bb5f3
Dongsh <dongsh315@gmail.com>
2015-12-11 15:20:42 +0800

    Initial Commit

---
+++ DrawImage_Lexter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DrawImage_Lexter
+//
+//  Created by Dongsh on 15/12/11.
+//  Copyright © 2015年 Dongsh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
